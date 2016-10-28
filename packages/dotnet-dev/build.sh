9127a44774f94dc2577f47711b10b91461f374d0
Christian Rondeau <christian.rondeau@gmail.com>
2016-10-28 17:07:46 -0400

    dotnet-dev now build through buildpackage.sh

--- packages/dotnet-dev/build.sh
+++ packages/dotnet-dev/build.sh
@@ -5,10 +5,13 @@ TERMUX_PKG_SRCURL=https://github.com/dotnet/cli/archive/rel/1.0.0.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=""
 TERMUX_PKG_RM_AFTER_INSTALL=""
 TERMUX_PKG_DEPENDS="libc6, libcurl3, libgcc1, libgssapi-krb5-2, libicu55, liblldb-3.6, liblttng-ust0, libssl1.0.0, libstdc++6, libstdc++6, libunwind8, libuuid1, zlib1g"
-TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_BUILD_IN_SRC="yes"
 TERMUX_PKG_FOLDERNAME="cli-rel-1.0.0"
 
-termux_step_host_build () {
+	# Might be unecessary. See: https://docs.microsoft.com/en-us/dotnet/articles/core/rid-catalog#linux-rids
+DOTNET_RUNTIME_ID="ubuntu.16.04-x64"
+
+termux_step_make() {
 	# Requires to be run from the src path
 	pushd $TERMUX_PKG_SRCDIR
 
@@ -17,14 +20,15 @@ termux_step_host_build () {
 	git commit -m "Termux Build" --allow-empty
 	git branch rel/$TERMUX_PKG_VERSION 
 
-	# Might be unecessary. See: https://docs.microsoft.com/en-us/dotnet/articles/core/rid-catalog#linux-rids
-	DOTNET_RUNTIME_ID="ubuntu.16.04-x64"
-
 	# Skip tests, otherwise it will take forever
 	./build.sh /t:Compile
 
 	# Might be unecessary. Returns the the working folder of Termux's build-package.sh
 	popd
+}
+
+termux_step_make_install() {
+	cp $TERMUX_PKG_SRCDIR/artifacts/$DOTNET_RUNTIME_ID/stage2/dotnet $TERMUX_PREFIX/bin/dotnet
 }
 
 termux_step_pre_configure() {
